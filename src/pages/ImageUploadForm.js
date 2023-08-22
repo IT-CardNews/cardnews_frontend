94aff6fd8a5777ab00991a89bf85c11cd28381ff
ch00me <anijam12@naver.com>
2023-08-22 10:25:12 +0900

    [MODIFY] 취업 정보 업로드 API

--- src/pages/ImageUploadForm.js
+++ src/pages/ImageUploadForm.js
@@ -20,7 +20,7 @@ const ImageUploadForm = () => {
     formData.append('jobField', jobFieldValue);
     formData.append('requirements', requirementsValue);
 
-    axios.post('http://localhost:5000/upload', formData)
+    axios.post('http://localhost:5000/upload_job', formData)
       .then(response => {
         // 업로드 성공 처리
       })
